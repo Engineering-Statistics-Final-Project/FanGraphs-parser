69f83ecc68cb05a75121821fc65e1e09021c32de
7070707070 <sky2502211@gmail.com>
2021-01-03 19:23:05 +0800

    Updata 2000-2019 Data

---
+++ parser.py
@@ -0,0 +1,62 @@
+import requests
+from bs4 import BeautifulSoup
+
+for month in range(2000,2020):
+    url_string = "https://www.fangraphs.com/leaders.aspx?pos=all&stats=pit&lg=all&qual=0&type=0&season="
+    url_month  = str(month)
+    url_string2 = "&month=0&season1="
+    url_string3 = "&ind=0&team=0,ts&rost=0&age=14,58&filter=&players=0&startdate=&enddate=&page=1_50"
+    __url__ = url_string+url_month+url_string2+url_month+url_string3
+
+    response = requests.get(__url__)
+    __html_parser__ = BeautifulSoup(response.text,"html.parser")
+
+    regular_str = "id=LeaderBoard1_dg1_ctl00__"
+    result = []
+
+    __file__ = "DATA/"+str(month)+"_tmp.txt" 
+    FILE = open(__file__,mode="w")
+
+    for idx in range(0,30):
+        tmp_class_bs4 = __html_parser__.find("tr",id=("LeaderBoard1_dg1_ctl00__"+str(idx)))
+        result.append(tmp_class_bs4)
+        FILE.write(str(tmp_class_bs4))
+        FILE.write("")
+
+    FILE.close()
+
+for month2 in range(2000,2020):
+    __file2__ = "DATA/"+str(month2)+"_tmp.txt" 
+    FILE2 = open(__file2__,mode="r")
+
+    __file_ans = "DATA/"+str(month2)+".txt"
+    FILE_ans = open(__file_ans,mode="w")
+
+    all_str = FILE2.readlines()
+    ans = []
+    
+    for i in range(0,30):
+        idx1 = i*2
+        idx2 = i*2+1
+        str1 = all_str[idx1]
+        str2 = all_str[idx2]
+        str1_len = len(str1)
+        str2_len = len(str2)
+        
+        for k in range(0,50):
+            flag = 0
+            __str__ = ""
+            for j in range(0,str2_len):
+                if(str2[j]=='<' and flag==k): break
+                if(flag==k): __str__=__str__+str2[j]
+                if(str2[j]=='>'): flag = flag +1
+            if(len(__str__)!=0):ans.append(__str__)
+
+    for i in range(0,30):
+        for j in range(1,24):
+            if(j==1): FILE_ans.write('{:15}'.format(ans[i*24+j]))
+            else: FILE_ans.write('{:8}'.format(ans[i*24+j]))
+        FILE_ans.write("\n")
+
+    FILE2.close()
+    FILE_ans.close()
